b9a753f5da37e04dc5cb7109908a60ceb389ccff
wersoam <cowistrash@gmail.com>
2023-05-18 10:16:03 -0400

    com

--- sql/src/App.vue
+++ sql/src/App.vue
@@ -1,28 +1,24 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { supabase } from "./lib/supabase.js";
-
+import helloworld from "./components/HelloWorld.vue";
 const countries = ref([]);
 
 async function getCountries() {
   const { data } = await supabase.from("countries").select();
   countries.value = data;
 }
-async function addCountry() {
-  const { data } = await supabase
-    .from("countries")
-    .insert({ id: 1, name: "Denmark" })
-    .select();
-  countries.value = data;
-}
-
 async function delCountry() {
-  const { data } = await supabase
+  const { data, error } = await supabase
     .from("countries")
-    .select()
-    .not("name", "is", null)
     .delete()
-    .eq("id", "name");
+    .eq("name", countries.name);
+  if (error) {
+    console.log(error);
+  }
+  if (data) {
+    console.log(data);
+  }
   countries.value = data;
 }
 
@@ -36,5 +32,6 @@ onMounted(() => {
     <div v-for="country in countries" :key="country.id">
       {{ country.name }} <button @click="delCountry()"></button>
     </div>
+    <helloworld />
   </div>
 </template>
