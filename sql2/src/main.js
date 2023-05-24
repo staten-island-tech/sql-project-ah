c4c13822e2921844f2d0aa3a7a4d991065aaffce
wersoam <cowistrash@gmail.com>
2023-05-24 10:16:08 -0400

    save
    continue on router

--- sql2/src/main.js
+++ sql2/src/main.js
@@ -1,29 +1,31 @@
 // main.js
+import { supabase } from '../client/supabase.js'
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import pageview from './views/pageview.vue'
+import loginview from './views/loginview.vue'
+import openview from './views/openview.vue'
 
-import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
-import App from "./App.vue";
-import pageview from "./views/pageview.vue";
-import loginview from "./views/loginview.vue";
-import openview from "./views/openview.vue";
 const routes = [
   {
-    path: "/",
-    component: openview,
+    path: '/',
+    component: openview
   },
   {
-    path: "/login",
-    component: loginview,
+    path: '/login',
+    component: loginview
   },
   {
-    path: "/page",
-    component: pageview,
-  },
-];
+    path: '/page',
+    component: pageview
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: routes,
-});
-
-createApp(App).use(router).mount("#app");
+  routes: routes
+})
+let localuser
+router.beforeEach(to,from,next)=>{if(to.meta.requiresAuth)}
+createApp(App).use(router).mount('#app')
--- sql2/src/views/loginview.vue
+++ sql2/src/views/loginview.vue
@@ -1,33 +1,67 @@
 <template>
-  <div>
+  <div class="container">
     <input type="email" placeholder="email" v-model="email" />
     <input type="password" placeholder="Password" v-model="password" />
-    <button @click="signUp()">sign up</button>
+    <button @click="signUp()">sign up</button><button @click="login()">login</button
+    ><button @click="logout()">log out</button>
+    <button @click="checksession()">check</button>
   </div>
 </template>
 
 <script setup>
-import { supabase } from "../client/supabase.js";
-import { ref } from "vue";
-const email = ref("");
-const password = ref("");
-
+import { supabase } from '../client/supabase.js'
+import { ref } from 'vue'
+const email = ref('')
+const password = ref('')
 async function signUp() {
   try {
     const { data, error } = await supabase.auth.signUp({
       email: email.value,
-      password: password.value,
-    });
+      password: password.value
+    })
     if (error) {
-      console.log(error);
+      console.log(error)
     } else {
-      console.log(data);
+      console.log(data)
+      ;(email.value = ''), (password.value = '')
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
-const names = ref([]);
+async function login() {
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email.value,
+      password: password.value
+    })
+    if (error) {
+      console.log(error)
+    } else {
+      console.log(data)
+      ;(email.value = ''), (password.value = '')
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+async function checksession() {
+  const currentuser = await supabase.auth.getSession()
+  console.log(currentuser.data.session)
+}
+async function logout() {
+  const { error } = await supabase.auth.signOut()
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('logged out')
+  }
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  border: 2px;
+  text-align: center;
+}
+</style>
